e93b9abcbc9e7e239c045bd282cb93c8158d1c5a
Mike <mike@sachsmedia.com>
2015-05-29 10:59:05 -0400

    svg fallback to png

--- wp-content/themes/sst/header.php
+++ wp-content/themes/sst/header.php
@@ -12,24 +12,23 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<script>
+		//Flag the body with "no-svg" class if the browser doesn't support SVG 
+		function hasSVG() {return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");}if ( !hasSVG() ) document.documentElement.className += " no-svg";
+	</script>
+
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri()?>/style.css" type="text/css" media="all">
-<!--
-	<script src="//use.typekit.net/lvk5xgy.js"></script>
-	<script>try{Typekit.load();}catch(e){}</script>
--->
-<!--<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'> -->
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<div class="white-bg">
 	<a class="skip-link screen-reader-text btn" href="#content">Skip to content</a>
-
-	<header class="site-header" role="banner">
+	<header class="site-header" id="js-header" role="banner">
 		<div class="nav-wrapper constrain">
 			<div class="site-branding">
-				<a href="/"><img src="<?php echo get_stylesheet_directory_uri()?>/img/sst-logo.svg" alt="Safer, Smarter Teens" title="Safer, Smarter Teens"/></a>
+				<a class="no-hover-line" href="<?= home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri()?>/img/sst-logo.svg" onerror="this.src='<?php echo get_stylesheet_directory_uri()?>/img/sst-logo.png'; this.onerror=null;" alt="Safer, Smarter Teens" title="Safer, Smarter Teens"/></a>
 			</div>
 	
 			<nav class="main-navigation" role="navigation">
