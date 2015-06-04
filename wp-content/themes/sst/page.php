5d7dc57dd630e9624b5a083a176730773a24df0f
Mike <mike@sachsmedia.com>
2015-06-04 18:40:50 -0400

    middle and high interior page template

--- wp-content/themes/sst/page.php
+++ wp-content/themes/sst/page.php
@@ -3,25 +3,59 @@
  * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package Spike
  */
 
 get_header(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+	
+	<?php 
+		// Choose the wp-menu of parent page's name or if no parent of page name
+		$menuToDisplay = $post->post_parent ? strtolower( get_the_title( $post->post_parent )) : $post->post_name;
+		
+		// Get ACF fields
+		$fields = get_fields();  
+		
+		// if there is no "Did You Know" block, get the featured image url
+		if ( !$fields['did_you_know'] ) {
+			$thumb_id = get_post_thumbnail_id();
+			$thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true)[0];
+		}
+	?>
+	
+	<div class="page-hero" <?php if ( !$fields['did_you_know'] ): ?>style="background-image: url('<?= $thumb_url; ?>')"<?php endif;?>>
+		<?php if ( ($fields['did_you_know']) ): ?>
+		<div class="constrain flex-center">
+			<div class="did-you-know">
+				<?= $fields['did_you_know']; ?>
+			</div>
+			<div class="lauren-image"></div>
+		</div>
+		<?php endif; ?>
+	</div>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-			<?php endwhile; // end of the loop. ?>
-
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="constrain">
+					<header class="entry-header">
+						<h1 class="entry-title brush"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
+				
+					<div class="entry-content">
+						<?php the_content();?>
+					</div><!-- .entry-content -->
+					
+					<footer class="entry-footer">
+						<?php edit_post_link( __( 'Edit', 'spike' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-footer -->
+				</div>
+			</article><!-- #post-## -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+	<?php endwhile; // end of the loop. ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
