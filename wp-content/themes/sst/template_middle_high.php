60611be50be77cfa866201b10b98012061551d77
Mike <mike@sachsmedia.com>
2015-06-26 17:43:19 -0400

    crossover button edit

--- wp-content/themes/sst/template_middle_high.php
+++ wp-content/themes/sst/template_middle_high.php
@@ -77,9 +77,9 @@ get_header(); ?>
 										<?= $fields['download']; ?>
 									</div>
 									<?php if (is_page('middle-school')): ?>
-										<a class=" crossover btn btn-blue" href="<?= home_url()?>/education/high-school">High School »</a> 
+										<a class=" crossover btn btn-blue" href="<?= home_url()?>/education/high-school">High School</a> 
 									<?php else: ?>
-										<a class="crossover btn btn-blue" href="<?= home_url()?>/education/middle-school">Middle School »</a>
+										<a class="crossover btn btn-blue" href="<?= home_url()?>/education/middle-school">Middle School</a>
 									<?php endif; ?>
 								</section>
 							</div>
